686120d1f9d03da67768dcc6d061fb770975f89e
samtranum <samtranum@gmail.com>
2025-11-21 12:53:21 +0000

    More fixes

--- app/models/council.rb
+++ app/models/council.rb
@@ -2,4 +2,8 @@ class Council < ApplicationRecord
   has_many :council_sessions
   has_many :councillors
   has_many :local_electoral_areas
+
+  def to_param
+    slug
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,6 @@
 Rails.application.routes.draw do
   root to: "landing#index"
 
-  scope "/:council_id" do
-    get "/", to: "home#show", as: :council_root
-
-    get "faq" => "pages#faq", :as => :faq
-
-    resources :corrections, only: [:new, :create] do
-      collection { get "thanks" }
-    end
-
-    resources :councillors, only: [:index, :show]
-    get "councillors/:id/:view(/:context)" => "councillors#show"
-
-    resources :local_electoral_areas, path: "areas", only: [:index, :show]
-    get "areas/:id/:view(/:context)" => "local_electoral_areas#show"
-
-    resources :parties, only: [:index, :show]
-    get "parties/:id/:view(/:context)" => "parties#show"
-
-    resources :meetings, only: [:index]
-    get "meetings/:meeting_type/:occurred_on" => "meetings#show", :as => :meeting_path
-    get "meetings/:meeting_type/:occurred_on/:view(/:context)" => "meetings#show"
-
-    resources :motions, only: [:index, :show]
-    get "motions/:id/:view(/:context)" => "motions#show"
-
-    resources :amendments, only: [:show]
-    get "amendments/:id/:view(/:context)" => "amendments#show"
-
-    resources :topics, only: [:index, :show]
-  end
-
   namespace :admin do
     root to: "dashboard#show"
     resources :councils, only: [:index, :new, :create]
@@ -71,6 +40,37 @@ Rails.application.routes.draw do
     resources :media_mentions, only: [:show, :destroy, :edit, :update]
 
     resources :corrections
+  end
+
+  scope "/:council_id" do
+    get "/", to: "home#show", as: :council_root
+
+    get "faq" => "pages#faq", :as => :faq
+
+    resources :corrections, only: [:new, :create] do
+      collection { get "thanks" }
+    end
+
+    resources :councillors, only: [:index, :show]
+    get "councillors/:id/:view(/:context)" => "councillors#show"
+
+    resources :local_electoral_areas, path: "areas", only: [:index, :show]
+    get "areas/:id/:view(/:context)" => "local_electoral_areas#show"
+
+    resources :parties, only: [:index, :show]
+    get "parties/:id/:view(/:context)" => "parties#show"
+
+    resources :meetings, only: [:index]
+    get "meetings/:meeting_type/:occurred_on" => "meetings#show", :as => :meeting_path
+    get "meetings/:meeting_type/:occurred_on/:view(/:context)" => "meetings#show"
+
+    resources :motions, only: [:index, :show]
+    get "motions/:id/:view(/:context)" => "motions#show"
+
+    resources :amendments, only: [:show]
+    get "amendments/:id/:view(/:context)" => "amendments#show"
+
+    resources :topics, only: [:index, :show]
   end
 
   get "/404" => "errors#not_found"
