3a29630a1cbc3b3c5b641240438e2334234afff9
samtranum <samtranum@gmail.com>
2025-11-28 14:38:41 +0000

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,18 @@ module ApplicationHelper
     when "changeofaffiliation"
       "Changed party affiliation from #{link_to event.eventable.outgoing_party.name, event.eventable.outgoing_party} to #{link_to event.eventable.incoming_party.name, event.eventable.incoming_party}"
     when "cooption"
-      "#{link_to event.eventable.outgoing_seat.councillor.full_name, event.eventable.outgoing_seat.councillor} resigned from the council and was replaced by #{link_to event.eventable.incoming_seat.councillor.full_name, event.eventable.incoming_seat.councillor}"
+      outgoing_seat = event.eventable.outgoing_seat
+      incoming_seat = event.eventable.incoming_seat
+      
+      if outgoing_seat&.councillor && incoming_seat&.councillor
+        "#{link_to outgoing_seat.councillor.full_name, outgoing_seat.councillor} resigned from the council and was replaced by #{link_to incoming_seat.councillor.full_name, incoming_seat.councillor}"
+      elsif outgoing_seat&.councillor
+        "#{link_to outgoing_seat.councillor.full_name, outgoing_seat.councillor} resigned from the council"
+      elsif incoming_seat&.councillor
+        "Co-opted to the council (replacing a vacant seat)"
+      else
+        "Co-option event (data incomplete)"
+      end
     else
       raise "Must implement summary for #{event.eventable_type}"
     end
