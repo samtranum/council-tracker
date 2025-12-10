880ef26cb5ba5775f77ab6e111b4e457b093a455
samtranum <samtranum@gmail.com>
2025-12-10 14:49:38 +0000

    Implement party color management with admin picker and database migration

---
+++ app/controllers/admin/parties_controller.rb
@@ -0,0 +1,29 @@
+class Admin::PartiesController < ApplicationController
+  before_action :require_admin
+  before_action :set_party, only: [:edit, :update]
+
+  def index
+    @parties = Party.by_name
+  end
+
+  def edit
+  end
+
+  def update
+    if @party.update(party_params)
+      redirect_to admin_parties_path, notice: "Party #{@party.name} updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_party
+    @party = Party.find(params[:id])
+  end
+
+  def party_params
+    params.require(:party).permit(:name, :color)
+  end
+end
