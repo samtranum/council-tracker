9cace516ea788713c6f8793cfc5aecff1aec0cc7
samtranum <samtranum@gmail.com>
2026-04-08 16:07:48 +0100

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,6 @@ class HomeController < ApplicationController
     @local_electoral_areas = @council_session.local_electoral_areas.by_name
     @parties = @council_session.parties.by_name
     @topics = Motion.published.pluck(:tags).flatten.uniq.sort
-    @recent_motions = current_council.motions.published.order(occurred_on: :desc).limit(10)
+    @recent_motions = current_council.motions.published.by_occurred_on.limit(10)
   end
 end
